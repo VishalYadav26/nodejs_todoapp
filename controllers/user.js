3cc617daedf9fbb1c2e81de2885b0b16b393e11a
VishalYadav26 <112235127+VishalYadav26@users.noreply.github.com>
2024-06-23 21:28:23 +0530

    modified

--- controllers/user.js
+++ controllers/user.js
@@ -31,7 +31,7 @@ export const login = async (req,res,next)=>{
 
 };
 
-export const register = async (req,res)=>{
+export const register = async (req,res,next)=>{
     try {
         const {name,email,password} = req.body;
         let user = await User.findOne({email});
@@ -45,7 +45,7 @@ export const register = async (req,res)=>{
         const hashedPassword = await bcrypt.hash(password, 10);
         user = await User.create({name,email,password:hashedPassword});
 
-    sendCookie(user,res,"Registered Successfully!",201);
+    sendCookie( user, res, "Registered Successfully!",201);
     } catch (error) {
         next(error);
     }
